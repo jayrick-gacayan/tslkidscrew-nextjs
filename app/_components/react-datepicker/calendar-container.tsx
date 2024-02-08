78633b12d4b35375ae3b4ea100b8254fbbff1add
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2024-02-08 20:56:16 +0800

    fix: added margin on datepicker container

--- app/_components/react-datepicker/calendar-container.tsx
+++ app/_components/react-datepicker/calendar-container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function calendarContainer({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full bg-white rounded border border-secondary-light shadow-lg">
+    <div className="-mt-[1px] w-full bg-white rounded border border-secondary-light shadow-lg">
       {children}
     </div>
   )
