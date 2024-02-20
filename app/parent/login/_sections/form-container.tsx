cdeace250d5b0f9db356ae6287ec6336b9b48616
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2024-02-20 10:17:12 +0800

    fix: added toast after success login for parent

--- app/parent/login/_sections/form-container.tsx
+++ app/parent/login/_sections/form-container.tsx
@@ -1,27 +1,52 @@
 'use client';
 
 import { useFormState } from 'react-dom';
-
-
 import LoginForm from '@/app/_components/login/login-form';
 import { roleLogin } from '@/actions/auth-actions';
 import LoginButtons from '@/app/_components/login/login-buttons';
 import RememberMe from '@/app/_components/login/remember-me';
-import { useRouter } from 'next/navigation';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { fieldInputValue } from '@/types/helpers/field-input-value';
 import { LoginFormStateProps } from '@/types/props/login-form-state-props';
+import { redirectToPath } from '@/actions/common-actions';
+import { toast, ToastContentProps } from 'react-toastify';
 
 export default function FormContainer() {
-  const router = useRouter();
   const formRef = useRef<HTMLFormElement>(null);
   const [state, formAction] = useFormState(
-    roleLogin.bind(null, '/admin/dashboard'),
+    roleLogin.bind(null, '/parent/dashboard'),
     {
       email: fieldInputValue<string>(''),
       password: fieldInputValue<string>(''),
     } as LoginFormStateProps
   );
+
+  useEffect(() => {
+    async function pathToRedirect(redirectTo: string) {
+      await redirectToPath(redirectTo);
+    }
+
+    if (state?.success) {
+      if (state.success && state?.redirectTo) {
+        toast((props: ToastContentProps<unknown>) => {
+          return (
+            <div className="text-black">
+              {state?.message}
+            </div>
+          )
+        }, {
+          toastId: `parent-login-success-${Date.now()}`,
+          type: 'success',
+          hideProgressBar: true,
+        });
+        formRef.current?.reset();
+        pathToRedirect(state.redirectTo)
+      }
+    }
+  }, [
+    state?.redirectTo,
+    state?.success,
+  ]);
 
   return (
     <form action={formAction} ref={formRef} className='space-y-4'>
