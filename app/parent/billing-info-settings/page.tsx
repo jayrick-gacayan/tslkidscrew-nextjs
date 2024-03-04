fa2a4e44a6f6998ff26f72a83432e7bdf26ff434
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2024-03-04 19:25:20 +0800

    feat: display customer information

--- app/parent/billing-info-settings/page.tsx
+++ app/parent/billing-info-settings/page.tsx
@@ -1,7 +1,15 @@
+import { auth } from "@/auth";
+import { Parent } from "@/models/parent";
+import { getCustomerInfo } from "@/services/parent-info-services";
+import { Session } from "next-auth";
 import BillingInfoSettingsTabContainer from "./_sections/billing-info-settings-tab-container";
 
-export default function Page() {
+export default async function Page() {
+  let parent: Session<Partial<Parent>> = await auth();
+
+  let result = await getCustomerInfo(parent.user?.customer_id?.toString()!, parent?.accessToken!)
+
   return (
-    <BillingInfoSettingsTabContainer />
+    <BillingInfoSettingsTabContainer parent={result.data ?? undefined} />
   )
 }
