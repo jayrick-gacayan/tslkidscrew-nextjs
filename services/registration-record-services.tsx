45093a72de444a93f9dcfa50b80858108dade9a0
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2024-03-19 19:14:13 +0800

    fix: added get all registration records

--- services/registration-record-services.tsx
+++ services/registration-record-services.tsx
@@ -1,3 +1,4 @@
+import { Paginate } from "@/models/paginate";
 import { RegistrationRecord } from "@/models/registration-record";
 import { Result } from "@/models/result";
 import { authHeaders } from "@/types/helpers/auth-headers";
@@ -21,6 +22,42 @@ export async function getRegistrationRecord(reg_id: string, token: string) {
 
   } catch (error) {
     return new Result<RegistrationRecord>({
+      response: undefined,
+      message: result.statusText,
+      statusCode: result.status
+    })
+  }
+}
+
+export async function getRegistrationRecords(token: string) {
+  let result = await fetch(
+    process.env.NEXT_PUBLIC_API_PARENT_URL! + `/customer_users/regis_records`,
+    { ...authHeaders(token) }
+  );
+
+  try {
+    let response = await result.json();
+
+    if (response.status === 200) {
+
+      return new Result<Paginate<any>>({
+        ...response,
+        data: {
+          data: response.registrations ?? [],
+          total: response.total_invoices ?? 1,
+        },
+        statusCode: response.status ?? result.status
+      })
+    }
+
+    return new Result<Paginate<any>>({
+      ...response,
+      data: undefined,
+      statusCode: response.status ?? result.status
+    })
+
+  } catch (error) {
+    return new Result<Paginate<any>>({
       response: undefined,
       message: result.statusText,
       statusCode: result.status
